0b0fc36634d270adce29dc0cfd5da4688a707a8b
Đạt Nguyễn <nguyendatz567@gmail.com>
2025-12-18 02:56:39 +0700

    tạo project

---
+++ app/(public)/layout.jsx
@@ -0,0 +1,16 @@
+'use client'
+import Banner from "@/components/Banner";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { ClerkProvider } from '@clerk/nextjs';
+export default function PublicLayout({ children }) {
+
+    return (
+            <>
+            <Banner />
+            <Navbar />
+            {children}
+            <Footer />
+            </>
+    );
+}
